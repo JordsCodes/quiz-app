3c76b701b4ddd45d140eb9ea9885c88444537d9c
Jordan <j.r.noble@live.co.uk>
2023-07-18 21:09:38 +0100

    merge fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import "../styles/app.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import GenerateQuiz from "../components/GenerateQuiz";
+import GenerateQuiz from "./GenerateQuiz";
 import LandingPage from "./LandingPage";
 import MultipleChoice from "./MultipleChoice";
 
@@ -10,7 +10,7 @@ const App = () => {
     <div className="app">
       <Routes>
         <Route path="generate-quiz" element={<GenerateQuiz />} />
-        <Route path="/" element={<LandingPage />} />  
+        <Route path="/" element={<LandingPage />} />
         <Route path="/multi-choice" element={<MultipleChoice />} />
         <Route path="/true-false" />
         <Route path="/quiz-end" />
