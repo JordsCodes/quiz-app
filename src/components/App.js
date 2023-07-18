b0ed998991ae9ba6a462277938a0c37a7f7947ef
Jordan <j.r.noble@live.co.uk>
2023-07-18 21:21:39 +0100

    merge conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,6 @@
 import "../styles/app.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import TrueFalse from "./TrueFalse";
-import QuizEnd from './QuizEnd';
-
-function App() {
-
-  return (
-    <div className="app">
-      <Routes>
-        <Route path="/" />
-        <Route path="/generate-quiz" />
-        <Route path="/multi-choice" />
-        <Route path="/true-false" element={<TrueFalse />} />
-        <Route path="/quiz-end" element={<QuizEnd />} />
-=======
 import GenerateQuiz from "./GenerateQuiz";
 import LandingPage from "./LandingPage";
 import MultipleChoice from "./MultipleChoice";
@@ -29,7 +14,6 @@ const App = () => {
         <Route path="/multi-choice" element={<MultipleChoice />} />
         <Route path="/true-false" />
         <Route path="/quiz-end" />
->>>>>>> main
       </Routes>
     </div>
   );
