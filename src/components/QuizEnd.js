4f86130dca1dde3ffd15c7a4d353067f7b1d8282
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-07-20 18:34:17 +0100

    updated files

--- src/components/QuizEnd.js
+++ src/components/QuizEnd.js
@@ -4,22 +4,24 @@ import { Link } from "react-router-dom";
 
 const QuizEnd = () => {
   return (
-    <>
+    <div className="quiz-end-container">
       <div className="congrats-message">
         <p>Congratulations!</p>
         <p>You scored x/y.</p>
       </div>
-      <Link to="/">
-        <button className="home-bttn" type="button">
-          Back to Home
-        </button>
-      </Link>
-      <Link to="/">
-        <button className="leader-board-bttn" type="button">
-          View Leader board
-        </button>
-      </Link>
-    </>
+      <div className="end-page-bttn-container">
+        <Link to="/">
+          <button className="end-page-bttn" type="button">
+            Back to Home
+          </button>
+        </Link>
+        <Link to="/">
+          <button className="end-page-bttn" type="button">
+            View Leader board
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- src/components/TrueFalse.js
+++ src/components/TrueFalse.js
@@ -3,20 +3,20 @@ import "../styles/true-false.css";
 
 const TrueFalse = () => {
   return (
-    <>
-      <div className="question">
+    <div className="true_false-container">
+      <div className="true_false-content">
         <h1 className="true-false-question">Question goes here</h1>
+        <button className="true-false-bttn" type="button">
+          True
+        </button>
+        <button className="true-false-bttn" type="button">
+          False
+        </button>
       </div>
-      <button className="true-bttn" type="button">
-        True
-      </button>
-      <button className="false-bttn" type="button">
-        False
-      </button>
       <button type="submit" className="next-bttn">
         Next
       </button>
-    </>
+    </div>
   );
 };
 
