887fc3d6fc287124c44b0e0f39bb16ff6c15fc83
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-07-26 15:46:00 +0100

    added a score for the end of the quiz, made it so you can accumulate correct answers

--- src/components/QuizEnd.js
+++ src/components/QuizEnd.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/quiz-end.css";
 import { Link } from "react-router-dom";
 
-const QuizEnd = () => {
+const QuizEnd = ({ score, totalQuestions }) => {
   return (
     <div className="end-page-content">
       <div className="congrats-message">
         <h2>Congratulations!</h2>
-        <h2>You scored X/Y.</h2>
+        <h2>
+          You scored {score}/{totalQuestions}.
+        </h2>
       </div>
       <div className="end-bttns-container">
         <Link to="/">
